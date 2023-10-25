3004e4b2f8bba81c39b6d50348a5a219db15cb75
miguelgargallo <it@itamaesan.org>
2023-10-25 22:06:37 +0200

    Postgres Update

--- update.sh
+++
@@ -1,2 +0,0 @@
-# Name the contianer "thecontainer"
-docker commit thecontainer new_image_name:latest && docker tag new_image_name:latest itamaesan/pylaros:latest && docker push itamaesan/pylaros:latest
